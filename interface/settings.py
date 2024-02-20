b3104a2b2c7c537275741c446687907636762afc
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2024-02-21 01:28:09 +0530

    change in the settings.py

--- interface/settings.py
+++ interface/settings.py
@@ -107,14 +107,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
+# settings.py
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'base/static'),
+    os.path.join(BASE_DIR, 'base', 'static'),
 ]
+
+
+
 
 # Media files (user uploaded files)
 MEDIA_URL = '/media/'
